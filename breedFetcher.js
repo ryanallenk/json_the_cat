055190a187e0a177885e439933e9660369180af8
ryanallenk <ryanallenk@gmail.com>
2022-03-17 17:37:16 -0400

    refactor to add callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,17 @@
 const request = require('request');
-const searchValue = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${searchValue}`, function (error, response, body) {
-  if (error) {
-    console.log(`Request failed: \n${error}`)
-    return
-  }
-  const data = JSON.parse(body);
-  if (data.length > 0) {
-    description = data[0]["description"];
-    console.log(description);
-    return
-  }
-  console.log("Error: that breed cannot be found.")
-});
+const fetchBreedDescription = function(breedName, callback) {
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data.length > 0) {
+      let description = data[0]["description"];
+      callback(null, description);
+    }
+  });
+};
 
-
+module.exports = { fetchBreedDescription };
